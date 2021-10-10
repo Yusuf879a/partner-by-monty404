1135858ad19799b0ab05950dbb7b07e6c47ae14e
Yusuf879a <92251595+Yusuf879a@users.noreply.github.com>
2021-10-10 13:59:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ message.member.voiceChannel.join()
 
 } else { 
 
-message.reply('** پێویستە تۆ لە ڤۆیسەکە بیت **'); 
+message.reply('** **'); 
 
 } 
 
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە lera Link Dane`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/HF9ZMmJHyK`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("id Channel ").send(
+  client.channels.get("896414745446469713").send(
 `> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
